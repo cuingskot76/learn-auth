3fac3d968973db5568ec460186e35e1a065e166b
cuingskot76 <afrizapamujo@gmail.com>
2023-01-16 20:27:21 +0700

    finish

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import User from "./model/user.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import verifyToken from "./middleware/auth.js";
 
 const app = express();
 
@@ -38,9 +39,13 @@ app.post("/register", async (req, res) => {
     });
 
     // create token
-    const token = jwt.sign({ user_id: user_id, email }, process.env.TOKEN_KEY, {
-      expiresIn: "2h",
-    });
+    const token = jwt.sign(
+      { user_id: user._id, email },
+      process.env.TOKEN_KEY,
+      {
+        expiresIn: "2h",
+      }
+    );
 
     // save user token
     user.token = token;
@@ -50,6 +55,45 @@ app.post("/register", async (req, res) => {
   } catch (error) {
     console.log(error);
   }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // check validate user input
+    if (!(email && password)) {
+      res.status(400).json({ msg: "Please fill all fields" });
+    }
+
+    // check if user already exist
+    const user = await User.findOne({ where: { email: email } });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      // create token
+      const token = jwt.sign(
+        { user_id: user._id, email },
+        process.env.TOKEN_KEY,
+        {
+          expiresIn: "2h",
+        }
+      );
+
+      // save user token
+      user.token = token;
+
+      // send response
+      res.status(200).json(user);
+    } else {
+      res.status(400).json({ msg: "Invalid credentials" });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.get("/welcome", verifyToken, (req, res) => {
+  res.status(200).send("Welcome 🙌 ");
 });
 
 export default app;
